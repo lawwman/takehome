3a6d7457debff413f7e677bbdbc0683b2ccc2cd4
lawwman <lawrenciok@gmail.com>
2020-06-09 15:12:50 +0800

    reactjs client side set up

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Container>
+      <h1>Testing</h1>
+      </Container>
+    );
+  }
+}
+
+
+export default App;
