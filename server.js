278cd145ccd24bd5df3f895814297d9f2ad21eed
lawwman <lawrenciok@gmail.com>
2020-06-09 16:01:27 +0800

    connected to local mongodb

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost/employees', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to local mongodb")).catch(() => console.log("Failed to connect to local mongodb"))
 
 const port = 5000
-
-
 
 app.listen(port, ()=> {
 	console.log(`Server started on PORT ${port}`)
